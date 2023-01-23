83d804e1e8b27138a4e8880ea07307b0f99d726a
Maja Wirkijowska <maja.wirkijowska@gmail.com>
2023-01-23 15:26:53 -0700

    initial project

---
+++ src/main/java/mg/thymeleafcourse/ThymeleafCourseApplication.java
@@ -0,0 +1,13 @@
+package mg.thymeleafcourse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThymeleafCourseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ThymeleafCourseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/mg/thymeleafcourse/ThymeleafCourseApplicationTests.java
@@ -0,0 +1,13 @@
+package mg.thymeleafcourse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThymeleafCourseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
